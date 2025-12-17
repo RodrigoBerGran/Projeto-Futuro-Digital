384453273858adcce91a0768142e74f4cbf5d4f5
Rodrigo <rodrigofry123@gmail.com>
2025-12-17 09:04:53 -0300

    paginas organizadas

--- Projeto/src/App.jsx
+++ Projeto/src/App.jsx
@@ -1,19 +1,20 @@
 import { useState, useEffect } from 'react'
-import { Routes, Route, Links } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
-import Dashboards from './pages/Dashboards.jsx'
-import Leads from './pages/Leads.jsx'
+import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
+import Leads from './pages/Leads.jsx'
+import Dashboards from './pages/Dashboards.jsx'
 
 function App() {
 
   return (
     <>
-    <Leads />
       <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Leads" element={<Leads />} />
         <Route path="/Dashboards" element={<Dashboards />} />
-        <Route path="/leads" element={<Leads />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
     </>
   )
---
+++ Projeto/src/pages/Login/Login.jsx
@@ -0,0 +1,12 @@
+function Login() {
+    return (
+        <>
+            <form>
+                <h1>Login</h1>
+                <label><input type="text" placeholder="Usuário" /></label>
+                <input type="password" placeholder="Senha" />
+            </form>
+        </>
+    )
+}
+export default Login
