143c0cb3c53292415043d3170b78367cea63ccbb
RodrigoBerGran <rodrigofry123@gmail.com>
2026-01-26 15:17:35 -0300

    deploy fix 2

--- Projeto/src/pages/NovoCliente/NovoCliente.jsx
+++ Projeto/src/pages/NovoCliente/NovoCliente.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { ClientesAPI } from '../../services/api/ClientesAPI';
+import { ClientesAPI } from '../../services/ClientesAPI';
 
 function NovoCliente() {
 
--- Projeto/src/pages/NovoUsuario/NovoUsuario.jsx
+++ Projeto/src/pages/NovoUsuario/NovoUsuario.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { UsuariosAPI } from '../../services/api/UsuariosAPI';
+import { UsuariosAPI } from '../../services/UsuariosAPI';
 
 function NovoUsuario() {
 
